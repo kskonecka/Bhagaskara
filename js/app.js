6c6e0fa3ebc62052c9a289e67a4f6d73482fd9fc
Kasia Skonecka <kasia.skonecka@gmail.com>
2016-04-19 15:57:34 +0200

    sticky menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
-  
+
+//STICKY MENU
+var nav = $('.menu');
+var navTopPosition = nav.offset().top;
+
+console.log(nav);
+console.log(navTopPosition);
+
+$(window).on('scroll', function(event){
+  setSticky();
+});
+
+$(window).on('resize', function(){
+   if(nav.hasClass('sticky')) {
+     navTopPosition = $('nav').offset().top;
+   } else {
+     navTopPosition = $('.menu').offset().top;
+   }
+
+  // setSticky();
+})
+
+function setSticky(){
+  var scrollTop = $(document).scrollTop();
+  console.log(scrollTop);
+
+  if (navTopPosition <= scrollTop) {
+    nav.addClass('sticky');
+  }else{
+    nav.removeClass('sticky')
+  }
+}
+
+
+
+
+
 });
