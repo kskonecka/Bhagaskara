09a22cb75bfb844ce2df28c2e2186b904d224867
Kasia Skonecka <kasia.skonecka@gmail.com>
2016-04-20 11:28:24 +0200

    js portfolio gallery fullscreen done

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 var nav = $('.menu');
 var navTopPosition = nav.offset().top;
 
-console.log(nav);
-console.log(navTopPosition);
+// console.log(nav);
+// console.log(navTopPosition);
 
 $(window).on('scroll', function(event){
   setSticky();
@@ -23,7 +23,7 @@ $(window).on('resize', function(){
 
 function setSticky(){
   var scrollTop = $(document).scrollTop();
-  console.log(scrollTop);
+  // console.log(scrollTop);
 
   if (navTopPosition <= scrollTop) {
     nav.addClass('sticky');
@@ -32,7 +32,34 @@ function setSticky(){
   }
 }
 
+//Portfolio Gallery
 
+var images = $('.portfolio-box').find('img');
+console.log(images);
+
+images.each(function(index, value){
+  $(this).on('click', function(){
+    var imageSource = $(this).attr('src');
+        console.log(imageSource);
+    var bigImageSource = $(this).data("bigImageSource");
+        console.log(bigImageSource);
+
+    var bigImage = $('<img>'); //create big image
+    var fullScreen = $('<div>'); // create div that contains big image
+    var closeButton = $('<div>'); //create button that exits full screen
+
+    fullScreen.addClass('fullScreen').appendTo($('.gallery-base'));//add fullScreen class and attach to <body>
+    bigImage.attr('src', bigImageSource).appendTo(fullScreen);//add img src and attach to <fullScreen>
+    closeButton.addClass('closeButton').appendTo(fullScreen);//add closeButton and attach to <fullScreen>
+    console.log(closeButton);
+
+    $('body').on('click', '.closeButton', function(){ //extis full screen
+        fullScreen.remove();
+    });
+
+  });
+
+});
 
 
 
