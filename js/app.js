8a4a22f6dafe8cb928ec927205408731b8c703ed
Kasia Skonecka <kasia.skonecka@gmail.com>
2016-04-21 16:01:19 +0200

    js progress-bar done

--- js/app.js
+++ js/app.js
@@ -80,61 +80,162 @@ images.each(function(index, value){
     $('body').on('click', '.closeButton', function(){ //extis full screen
         fullScreen.remove();
     });
+  });//end of images.this.event
+}); //end of images.each function
 
-  });
 
+//******************************************************************************************
+//opacity
+function progressBar(){
 
+var redford = $('.redford'); //definiuje zmienne elementow o klasie redford, newman i nolte
+var newman = $('.newman');
+var nolte = $('.nolte');
+var opacity = redford.css('opacity'); //to jest to opacity z css-a
 
+var progressBar1 = $("#bar1"); //to sa spany progres barow
+var progressBar2 = $("#bar2");
+var progressBar3 = $("#bar3");
+var progressBar4 = $("#bar4");
+
+redford.on('mouseenter', function(){ //to zmienia opacity na ruch myszy
+  redford.css('opacity', 1); //opacity 1
+  newman.css('opacity', opacity); //a pozostale elementy wracaja do ccs-owego opacity
+  nolte.css('opacity', opacity);
 });
 
-//filters********************************
-// var allIcon = $('.portfolio-filter-icon-1');
-// var cityIcon = $('.portfolio-filter-icon-2');
-// var waterIcon = $('.portfolio-filter-icon-3');
-// var natureIcon = $('.portfolio-filter-icon-4');
-// console.log(allIcon, cityIcon, waterIcon, natureIcon);
-// var filterIcons = $('.portfolio-filter-icons').find('div');
-//
-//
-//
-// var tags = images.data('filter');
-// console.log(tags);
+newman.on('mouseenter', function (){
+  newman.css('opacity', 1);
+  redford.css('opacity', opacity);
+  nolte.css('opacity', opacity);
+});
 
-// for (var i = 0; i < images.length; i++) {
-//   images[i].dataset.filter;
-//   console.log(images[i].dataset.filter);
-// }
+nolte.on('mouseenter', function (){//to zmienia opacity na ruch myszy
+  nolte.css('opacity', 1);
+  newman.css('opacity', opacity);
+  redford.css('opacity', opacity);
+});
+
+  redford.on('click', function(){ //na klika
+    redford.css('opacity', 1); //dzieje sie to co w mouseenter
+    newman.css('opacity', opacity);
+    nolte.css('opacity', opacity);
+
+    var wdWidth = $(this).data('wd'); //pobieram data-sety szerokosci skillsow
+    var gdWidth = $(this).data('gd');
+    var htmlWidth = $(this).data('html');
+    var uiuxWidth = $(this).data('uiux');
+
+    $('#wd-percent').text(""); //resetuje napis procentow
+    $('#gd-percent').text("");
+    $('#html-percent').text("");
+    $('#uiux-percent').text("");
+
+    progressBar1.css('width', 0); //resetuje szerokosci progressbaru
+    progressBar2.css('width', 0);
+    progressBar3.css('width', 0);
+    progressBar4.css('width', 0);
+
+    $('#wd-percent').text(wdWidth); //nastawiam napisy procentow progresbaru
+    $('#gd-percent').text(gdWidth);
+    $('#html-percent').text(htmlWidth);
+    $('#uiux-percent').text(uiuxWidth);
 
 
-// for (var i = 0; i < filterIcons.length; i++) {
-//
-//   filterIcons[i].addEventListener('click', function(event){
-//
-//   var filterIconsClasses = filterIcons[i].classList;
-//   console.log(filterIconsClasses);
-//   images.each(function(index, value){
-//     var imgFilters = $(this).data('filter');
-//     console.log(imgFilters);
-//     // if (imgFilters.indexOf(filterIconsClasses[1])
-//   });
-// });
-//
-//
-// }
-//
-// filterIcons.each(function(index, value){
-//   var filterIconsClasses = $(this).attr("class").split(' ');
-//   console.log(filterIconsClasses);
-//   filterIcons.
-//   $(this).on('click', function(){
-//     console.log("dziala");
-//
-//
-//
-//   });
+    progressBar1.animate({ //nastawiam szerokosci progresbarow
+      width: wdWidth
+    },1000);
+    progressBar2.animate({
+      width: gdWidth
+    },1000);
+    progressBar3.animate({
+      width: htmlWidth
+    },1000);
+    progressBar4.animate({
+      width: uiuxWidth
+    },1000);
+  });
 
-//});end of filterIcons event
+    newman.on('click', function (){
+      newman.css('opacity', 1);
+      redford.css('opacity', opacity);
+      nolte.css('opacity', opacity);
 
+      var wdWidth = $(this).data('wd');
+      var gdWidth = $(this).data('gd');
+      var htmlWidth = $(this).data('html');
+      var uiuxWidth = $(this).data('uiux');
+
+      $('#wd-percent').text("");
+      $('#gd-percent').text("");
+      $('#html-percent').text("");
+      $('#uiux-percent').text("");
+
+      progressBar1.css('width', 0);
+      progressBar2.css('width', 0);
+      progressBar3.css('width', 0);
+      progressBar4.css('width', 0);
+
+      $('#wd-percent').text(wdWidth);
+      $('#gd-percent').text(gdWidth);
+      $('#html-percent').text(htmlWidth);
+      $('#uiux-percent').text(uiuxWidth);
+
+      progressBar1.animate({
+        width: wdWidth
+      },1000);
+      progressBar2.animate({
+        width: gdWidth
+      },1000);
+      progressBar3.animate({
+        width: htmlWidth
+      },1000);
+      progressBar4.animate({
+        width: uiuxWidth
+      },1000);
+    });
+
+      nolte.on('click', function (){
+        nolte.css('opacity', 1);
+        newman.css('opacity', opacity);
+        redford.css('opacity', opacity);
+        var wdWidth = $(this).data('wd');
+        var gdWidth = $(this).data('gd');
+        var htmlWidth = $(this).data('html');
+        var uiuxWidth = $(this).data('uiux');
+
+        $('#wd-percent').text("");
+        $('#gd-percent').text("");
+        $('#html-percent').text("");
+        $('#uiux-percent').text("");
+
+        progressBar1.css('width', 0);
+        progressBar2.css('width', 0);
+        progressBar3.css('width', 0);
+        progressBar4.css('width', 0);
+
+        $('#wd-percent').text(wdWidth);
+        $('#gd-percent').text(gdWidth);
+        $('#html-percent').text(htmlWidth);
+        $('#uiux-percent').text(uiuxWidth);
+
+
+        progressBar1.animate({
+          width: wdWidth
+        },1000);
+        progressBar2.animate({
+          width: gdWidth
+        },1000);
+        progressBar3.animate({
+          width: htmlWidth
+        },1000);
+        progressBar4.animate({
+          width: uiuxWidth
+        },1000);
+      });
+
+}
+progressBar();
 
 
 
